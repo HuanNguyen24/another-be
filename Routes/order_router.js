bc3e185422d69cf7f799a31488e78e4f9fc6e0de
DoanJackson <anhnonono0@gmail.com>
2024-11-16 22:19:14 +0700

    Add api to get revenue many dates, get total each category

--- Routes/order_router.js
+++ Routes/order_router.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { decodeJWT, queryUser } from '#middlewares/authorization_middleware.js';
-import { updateOrder, createOrder, getOrderByTableId, getAllOrders } from '#controllers/order_controller.js';
+import { updateOrder, createOrder, getOrderByTableId, getAllOrders, calculateRevenueDates, getValuesCategory} from '#controllers/order_controller.js';
 
 const router = Router();
 
@@ -9,5 +9,8 @@ router.post('/', queryUser, createOrder);
 router.put('/:id', queryUser, updateOrder);
 router.get('/', getOrderByTableId);
 router.get('/all', getAllOrders);
+
+router.get('/revenueManyDates', calculateRevenueDates)
+router.get('/categoryValues', getValuesCategory)
 
 export default router;
